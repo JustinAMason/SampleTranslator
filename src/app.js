f54f0ba7abc98b448ca701e9c1428c74c54422f5
Justin Mason <justinmason@172-18-45-35.DYNAPOOL.NYU.EDU>
2018-11-15 17:46:58 -0500

    create homepage view

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + "/views");
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (request, response) => {
     response.render("index.hbs", {
