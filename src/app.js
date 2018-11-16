e24874d64933d91803c405b36e3e00cce6763a00
Justin Mason <justinmason@172-18-45-35.DYNAPOOL.NYU.EDU>
2018-11-15 19:58:56 -0500

    Set instructions to run web application on local machine

--- src/app.js
+++ src/app.js
@@ -3,14 +3,17 @@ const path = require('path');
 const ISO6391 = require('iso-639-1');
 const {Translate} = require('@google-cloud/translate');
 
-const projectId = 'translator-222623';
+if (!process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+    // To obtain credentials, go to https://cloud.google.com/translate/docs/quickstart
+    process.env.GOOGLE_APPLICATION_CREDENTIALS = "ENTER THE PATH TO YOUR GOOGLE APPLICATION CREDENTIALS HERE";
+}
+
+const projectId = 'translator-222623'; // Change this to the projectId listed in your credentials json file
 const translate = new Translate({projectId: projectId});
 
 const app = express();
-
 app.set('view engine', 'hbs');
 app.set('views', __dirname + "/views");
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 
